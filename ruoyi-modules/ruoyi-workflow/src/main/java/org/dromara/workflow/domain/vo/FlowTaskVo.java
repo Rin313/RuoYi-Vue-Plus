a8de11d414c101827141c893c9908fb1d03b230e
gssong <1742057357@qq.com>
2025-03-03 21:02:03 +0800

    add 添加获取节点接口

--- ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/domain/vo/FlowTaskVo.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/domain/vo/FlowTaskVo.java
@@ -174,6 +174,11 @@ public class FlowTaskVo implements Serializable {
     private String createByName;
 
     /**
+     * 是否为申请人节点
+     */
+    private boolean applyNode;
+
+    /**
      * 按钮权限
      */
     private List<ButtonPermission> buttonList;
