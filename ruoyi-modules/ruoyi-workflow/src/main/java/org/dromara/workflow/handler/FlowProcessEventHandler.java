070a1ad5309937bef862b41c967ee17314a9d3d8
疯狂的狮子Li <15040126243@163.com>
2025-04-09 11:53:40 +0800

    update 优化 调整监听器事件参数代码

--- ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/handler/FlowProcessEventHandler.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/handler/FlowProcessEventHandler.java
@@ -5,6 +5,7 @@ import org.dromara.common.core.domain.event.ProcessCreateTaskEvent;
 import org.dromara.common.core.domain.event.ProcessDeleteEvent;
 import org.dromara.common.core.domain.event.ProcessEvent;
 import org.dromara.common.core.utils.SpringUtils;
+import org.dromara.warm.flow.core.entity.Instance;
 import org.dromara.workflow.common.ConditionalOnEnable;
 import org.springframework.stereotype.Component;
 
@@ -25,25 +26,22 @@ public class FlowProcessEventHandler {
      * 总体流程监听(例如: 草稿，撤销，退回，作废，终止，已完成，单任务完成等)
      *
      * @param flowCode   流程定义编码
-     * @param businessId 业务id
-     * @param nodeType   节点类型（0开始节点 1中间节点 2结束节点 3互斥网关 4并行网关）
-     * @param nodeCode   流程节点编码
-     * @param nodeName   流程节点名称
+     * @param instance   实例数据
      * @param status     流程状态
      * @param params     办理参数
      * @param submit     当为true时为申请人节点办理
      */
-    public void processHandler(String flowCode, String businessId, Integer nodeType, String nodeCode, String nodeName,
+    public void processHandler(String flowCode, Instance instance,
                                String status, Map<String, Object> params, boolean submit) {
 
-        log.info("【流程事件发布】流程编码: {}, 业务ID: {}, 状态: {}, 节点类型: {}, 节点编码: {}, 节点名称: {}, 是否申请人节点: {}, 参数: {}", flowCode, businessId, status, nodeType, nodeCode, nodeName, submit, params);
+        log.info("【流程事件发布】流程编码: {}, 业务ID: {}, 状态: {}, 节点类型: {}, 节点编码: {}, 节点名称: {}, 是否申请人节点: {}, 参数: {}", flowCode, instance.getBusinessId(), status, instance.getNodeType(), instance.getNodeCode(), instance.getNodeName(), submit, params);
 
         ProcessEvent processEvent = new ProcessEvent();
         processEvent.setFlowCode(flowCode);
-        processEvent.setBusinessId(businessId);
-        processEvent.setNodeType(nodeType);
-        processEvent.setNodeCode(nodeCode);
-        processEvent.setNodeName(nodeName);
+        processEvent.setBusinessId(instance.getBusinessId());
+        processEvent.setNodeType(instance.getNodeType());
+        processEvent.setNodeCode(instance.getNodeCode());
+        processEvent.setNodeName(instance.getNodeName());
         processEvent.setStatus(status);
         processEvent.setParams(params);
         processEvent.setSubmit(submit);
@@ -54,21 +52,18 @@ public class FlowProcessEventHandler {
      * 执行创建任务监听
      *
      * @param flowCode   流程定义编码
-     * @param nodeType   节点类型（0开始节点 1中间节点 2结束节点 3互斥网关 4并行网关）
-     * @param nodeCode   流程节点编码
-     * @param nodeName   流程节点名称
+     * @param instance   实例数据
      * @param taskId     任务id
-     * @param businessId 业务id
      */
-    public void processCreateTaskHandler(String flowCode, String nodeCode, Long taskId, String businessId) {
+    public void processCreateTaskHandler(String flowCode, Instance instance, Long taskId) {
         log.info("发布流程任务事件, 流程编码: {}, 节点编码: {}, 任务ID: {}, 业务ID: {}", flowCode, nodeCode, taskId, businessId);
         ProcessCreateTaskEvent processCreateTaskEvent = new ProcessCreateTaskEvent();
         processCreateTaskEvent.setFlowCode(flowCode);
-        processCreateTaskEvent.setNodeType(nodeType);
-        processCreateTaskEvent.setNodeCode(nodeCode);
-        processCreateTaskEvent.setNodeName(nodeName);
+        processCreateTaskEvent.setBusinessId(instance.getBusinessId());
+        processCreateTaskEvent.setNodeType(instance.getNodeType());
+        processCreateTaskEvent.setNodeCode(instance.getNodeCode());
+        processCreateTaskEvent.setNodeName(instance.getNodeName());
         processCreateTaskEvent.setTaskId(taskId);
-        processCreateTaskEvent.setBusinessId(businessId);
         SpringUtils.context().publishEvent(processCreateTaskEvent);
     }
 
