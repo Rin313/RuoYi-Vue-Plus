c9f8eca1d1f627e33179df617bba0547d669a427
Rin313 <61936230+Rin313@users.noreply.github.com>
2025-12-08 19:30:12 +0800

    移除自动注入对BaseEntity的绑定

--- ruoyi-common/ruoyi-common-mybatis/src/main/java/org/dromara/common/mybatis/handler/InjectionMetaObjectHandler.java
+++ ruoyi-common/ruoyi-common-mybatis/src/main/java/org/dromara/common/mybatis/handler/InjectionMetaObjectHandler.java
@@ -1,14 +1,11 @@
 package org.dromara.common.mybatis.handler;
 
-import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.http.HttpStatus;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.dromara.common.core.domain.model.LoginUser;
 import org.dromara.common.core.exception.BizException;
-import org.dromara.common.core.utils.ObjectUtils;
-import org.dromara.common.mybatis.core.domain.BaseEntity;
 import org.dromara.common.satoken.utils.LoginHelper;
 
 import java.util.Date;
@@ -16,72 +13,31 @@ import java.util.Date;
 @Slf4j
 public class InjectionMetaObjectHandler implements MetaObjectHandler {
 
-    /**
-     * 插入填充方法，用于在插入数据时自动填充实体对象中的创建时间、更新时间、创建人、更新人等信息
-     *
-     * @param metaObject 元对象，用于获取原始对象并进行填充
-     */
     @Override
     public void insertFill(MetaObject metaObject) {
         try {
-            if (ObjectUtil.isNotNull(metaObject) && metaObject.getOriginalObject() instanceof BaseEntity baseEntity) {
-                // 获取当前时间作为创建时间和更新时间，如果创建时间不为空，则使用创建时间，否则使用当前时间
-                Date current = ObjectUtils.notNull(baseEntity.getCreateTime(), new Date());
-                baseEntity.setCreateTime(current);
-                baseEntity.setUpdateTime(current);
+            Date current = new Date();
+            // strictInsertFill 默认只在字段值为 null 时填充
+            this.strictInsertFill(metaObject, "createTime", Date.class, current);
+            this.strictInsertFill(metaObject, "updateTime", Date.class, current);
 
-                // 如果创建人为空，则填充当前登录用户的信息
-                if (ObjectUtil.isNull(baseEntity.getCreateBy())) {
-                    LoginUser loginUser = getLoginUser();
-                    if (ObjectUtil.isNotNull(loginUser)) {
-                        Long userId = loginUser.getUserId();
-                        // 填充创建人
-                        baseEntity.setCreateBy(userId);
-                    } 
-                }
-            } else {
-                Date date = new Date();
-                this.strictInsertFill(metaObject, "createTime", Date.class, date);
-                this.strictInsertFill(metaObject, "updateTime", Date.class, date);
+            LoginUser loginUser = LoginHelper.getLoginUser();
+            if (loginUser != null) {
+                Long userId = loginUser.getUserId();
+                this.strictInsertFill(metaObject, "createBy", Long.class, userId);
             }
         } catch (Exception e) {
             throw new BizException("自动注入异常 => " + e.getMessage(), HttpStatus.HTTP_UNAUTHORIZED);
         }
     }
 
-    /**
-     * 更新填充方法，用于在更新数据时自动填充实体对象中的更新时间和更新人信息
-     *
-     * @param metaObject 元对象，用于获取原始对象并进行填充
-     */
     @Override
     public void updateFill(MetaObject metaObject) {
         try {
-            if (ObjectUtil.isNotNull(metaObject) && metaObject.getOriginalObject() instanceof BaseEntity baseEntity) {
-                // 获取当前时间作为更新时间，无论原始对象中的更新时间是否为空都填充
-                Date current = new Date();
-                baseEntity.setUpdateTime(current);
-            } else {
-                this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
-            }
+            // 更新时间 - 始终覆盖
+            this.setFieldValByName("updateTime", new Date(), metaObject);
         } catch (Exception e) {
             throw new BizException("自动注入异常 => " + e.getMessage(), HttpStatus.HTTP_UNAUTHORIZED);
         }
     }
-
-    /**
-     * 获取当前登录用户信息
-     *
-     * @return 当前登录用户的信息，如果用户未登录则返回 null
-     */
-    private LoginUser getLoginUser() {
-        LoginUser loginUser;
-        try {
-            loginUser = LoginHelper.getLoginUser();
-        } catch (Exception e) {
-            return null;
-        }
-        return loginUser;
-    }
-
 }
